d6c92414be6230c1aa9e319b50bc4511aba177d9
zhangyu <zhangyu34@gmail.com>
2025-11-11 21:36:05 +0800

    use min heap

--- src/algo_project/c8_heapq.py
+++ src/algo_project/c8_heapq.py
@@ -1,24 +1,28 @@
-import heapq
+
 from collections import Counter
+import heapq
+
 
 class Pair:
     def __init__(self, str, freq):
         self.str = str
         self.freq = freq
-    
+        
     def __lt__(self, other):
         if self.freq == other.freq:
-            return self.str < other.str
-        return self.freq > other.freq
-        
-        
-        
+            return self.str > other.str
+        return self.freq < other.freq
+
 def k_most_frequent_strings(strs, k):
-    words_count = Counter(strs)
     heap = []
+    words_count = Counter(strs)
+    
     for str, freq in words_count.items():
         pair = Pair(str, freq)
         heapq.heappush(heap, pair)
+        if len(heap) > k:
+            heapq.heappop(heap)
         
-    result = [heapq.heappop(heap).str for _ in range(k) if len(heap) > 0 ]
+    result = [pair.str for pair in heap]
+    result.reverse()
     return result
