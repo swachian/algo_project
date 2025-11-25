10cd1f6791979396de9303952c145e4dd274746f
zhangyu <zhangyu34@gmail.com>
2025-11-25 11:15:03 +0800

    w48 redo

--- src/algo_project/c13_graph2.py
+++ src/algo_project/c13_graph2.py
@@ -6,34 +6,28 @@ class GraphNode:
         self.neighbors = []
 
 def shortest_transformation_sequence(start, end, dictionary):
-    # 0. 检查start和end在不在 dictionary
-    # 1. 转换成set
-    # 2. 找到start
-    # 3. 用在word L各个位置遍历替换26个字母的办法找到潜在的下家，并记录在queue里，该下家必须未出现在visited中过
-    # 4. 把word加入 visited
-    # 5. dist++, 如果找到了终点，则返回dist
+    # 遍历各个位置上的26个字母的可能性，找出在dictionary且不在visited的中的单词，放入queue后继续遍历，直到遍历完或者找到end
     words = set(dictionary)
-    abc = "abcdefghijklmnopqrstuvwxyz"
-    if start not in dictionary or end not in dictionary:
+    if start not in words or end not in words:
         return 0
-    if start == end:
-        return 1
-    queue = deque([start])
     visited = set()
+    abc = "abcdefghijklmnopqrstuvwxyz"
     dist = 0
+    queue = deque([start])
+    
     while queue:
         level_size = len(queue)
-        for l in range(level_size):
-            if l == 0:
-                dist += 1
+        dist += 1
+    
+        for n in range(level_size):
             node = queue.popleft()
-            visited.add(node)
             if node == end:
                 return dist
             for i in range(len(node)):
                 for c in abc:
-                    next_word = node[0:i] + c + node[i+1:]
-                    if next_word in words and next_word not in visited:
-                        queue.append(next_word) 
+                    word = node[0:i] + c + node[i+1:]
+                    if word not in visited and word in words:
+                        queue.append(word)
+            visited.add(node)
             
     return 0
